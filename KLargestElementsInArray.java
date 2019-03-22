ffc6092d4d5d1f8527d2c395d1fb59c3a940119c
Sheeyam Shellvacumar <sheeyam.shellvacumar@gmail.com>
2019-03-21 19:40:53 -0500

    Update KLargestElementsInArray.java

--- KLargestElementsInArray.java
+++ KLargestElementsInArray.java
@@ -7,19 +7,19 @@ public class KLargestElementsInArray {
 		// Number Array
 		Integer[] numArray = new Integer[] {23, 56, 2, 7, 14};
     
-    //K-Times
+    		//K-Times
 		int k = 3;
 		
-    // NumArray On Descending Order
-		Integer[] ascNumArray = SortArrayDESC(numArray);
+    		// NumArray On Descending Order
+		Integer[] descNumArray = SortArrayDESC(numArray);
 		
-    //Loop through the first k values and print.
+   		 //Loop through the first k values and print.
 		for(int x = 0; x < k; x++) {
-			System.out.println(ascNumArray[x]);
+			System.out.println(descNumArray[x]);
 		}
 	}
 	
-  // Sort Array in Descending Order
+  	// Sort Array in Descending Order
 	public static Integer[] SortArrayDESC(Integer[] array) {
 		Arrays.sort(array, Collections.reverseOrder());
 		return array;
