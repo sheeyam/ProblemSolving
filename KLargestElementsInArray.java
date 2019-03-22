7d9c4a438c18bab3d9943043bbc404695f4fd4c6
Sheeyam Shellvacumar <sheeyam.shellvacumar@gmail.com>
2019-03-21 19:36:59 -0500

    Create KLargestElementsInArray.java

---
+++ KLargestElementsInArray.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class KLargestElementsInArray {
+
+	public static void main(String[] args) {
+		// Number Array
+		Integer[] numArray = new Integer[] {23, 56, 2, 7, 14};
+    
+    //K-Times
+		int k = 3;
+		
+    // NumArray On Descending Order
+		Integer[] ascNumArray = SortArrayDESC(numArray);
+		
+    //Loop through the first k values and print.
+		for(int x = 0; x < k; x++) {
+			System.out.println(ascNumArray[x]);
+		}
+	}
+	
+  // Sort Array in Descending Order
+	public static Integer[] SortArrayDESC(Integer[] array) {
+		Arrays.sort(array, Collections.reverseOrder());
+		return array;
+	}
+}
